3dc135d4755b2db770588b2dacf52b44a28aeedb
taransm <67748452+taransm@users.noreply.github.com>
2020-07-08 23:23:09 +0530

    Updates

---
+++ precache-manifest.4a93ddf331b13127bee294fba2bfdab1.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e5c8d38be0f3a43e8425d10f612fab6",
+    "url": "/robot-react-web-app/index.html"
+  },
+  {
+    "revision": "abcf402c9619777cd68e",
+    "url": "/robot-react-web-app/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "85931c4057682aeedc47",
+    "url": "/robot-react-web-app/static/css/main.ad95f985.chunk.css"
+  },
+  {
+    "revision": "abcf402c9619777cd68e",
+    "url": "/robot-react-web-app/static/js/2.cab51068.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robot-react-web-app/static/js/2.cab51068.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "85931c4057682aeedc47",
+    "url": "/robot-react-web-app/static/js/main.66786ee8.chunk.js"
+  },
+  {
+    "revision": "b3bf2ce006ad8329e3c1",
+    "url": "/robot-react-web-app/static/js/runtime-main.a04f2b24.js"
+  },
+  {
+    "revision": "d786c3644f681705f252e9e3c4257333",
+    "url": "/robot-react-web-app/static/media/Holitter Hollow.d786c364.woff"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robot-react-web-app/static/media/SEGA.12a3db8b.woff"
+  }
+]);
